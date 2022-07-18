a894add09f68d5b0f4b8c4f14af40d7cc166a18c
ankittsharmaa <ankitbalotra1@gmail.com>
2022-07-18 21:46:48 +0530

    Correct auth files

--- src/Login.js
+++ src/Login.js
@@ -1,69 +1,82 @@
-import React, { useState } from 'react';
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "./firebase";
 
 function Login() {
-    const history = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const history = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const signIn = e => {
-        e.preventDefault();
+  const signIn = (e) => {
+    e.preventDefault();
 
-        auth
-            .signInWithEmailAndPassword(email, password)
-            .then(auth => {
-                history.push('/')
-            })
-            .catch(error => alert(error.message))
-    }
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    const register = e => {
-        e.preventDefault();
+  const register = (e) => {
+    e.preventDefault();
 
-        auth
-            .createUserWithEmailAndPassword(email, password)
-            .then((auth) => {
-                // it successfully created a new user with email and password
-                if (auth) {
-                    history.push('/')
-                }
-            })
-            .catch(error => alert(error.message))
-    }
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // it successfully created a new user with email and password
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    return (
-        <div className='login'>
-            <Link to='/'>
-                <img
-                    className="login_logo"
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
-                />
-            </Link>
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login_logo"
+          src="https://www.linkpicture.com/q/1657879166376_1.png"
+        />
+      </Link>
 
-            <div className='login_container'>
-                <h1>Sign-in</h1>
+      <div className="login_container">
+        <h1>Sign-in</h1>
 
-                <form>
-                    <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-                    <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-                    <button type='submit' onClick={signIn} className='login_signInButton'>Sign In</button>
-                </form>
+          <button type="submit" onClick={signIn} className="login_signInButton">
+            Sign In
+          </button>
+        </form>
 
-                <p>
-                    By signing-in you agree to the UrShop Conditions of Use & Sale. Please
-                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
-                </p>
+        <p>
+          By signing-in you agree to the UrShop Conditions of Use & Sale. Please
+          see our Privacy Notice, our Cookies Notice and our Interest-Based Ads
+          Notice.
+        </p>
 
-                <button onClick={register} className='login_registerButton'>Create UrShop Account</button>
-            </div>
-        </div>
-    )
+        <button onClick={register} className="login_registerButton">
+          Create UrShop Account
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
